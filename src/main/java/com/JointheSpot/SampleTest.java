a18c26c42b28819ce715036f059e8fcf6820a553
ANKYIT21 <sanjeetbhandari21@gmail.com>
2023-08-21 01:42:00 +0545

    last try

--- src/main/java/com/JointheSpot/SampleTest.java
+++ src/main/java/com/JointheSpot/SampleTest.java
@@ -6,23 +6,42 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import java.net.URL;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
+
+import static org.junit.Assert.assertTrue;
 
 public class SampleTest {
+
     private WebDriver driver;
 
+    @Before
     public void setUp() throws Exception {
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setCapability("browser", "Chrome");
         caps.setCapability("browser_version", "latest");
         caps.setCapability("os", "Windows");
         caps.setCapability("os_version", "10");
-        caps.setCapability("browserstack.appstore_distribution", "public"); // Add this line
+        caps.setCapability("browserstack.appstore_distribution", "public");
 
         // Initialize WebDriver for Chrome on Windows
         driver = new RemoteWebDriver(new URL("https://hub.browserstack.com/wd/hub"), caps);
     }
 
-
+    @Test
     public void testJoinTheSpot() {
         // Navigate to jointhespot.com
         driver.get("https://dev.d1oh6kkp7wvoqq.amplifyapp.com");
@@ -32,20 +51,15 @@ public class SampleTest {
         element.click();
         System.out.println("Element clicked");
 
+        // Add assertions or verifications here
+        // For example:
+        assertTrue("Element should be displayed after clicking", element.isDisplayed());
     }
 
+    @After
     public void tearDown() {
         if (driver != null) {
             driver.quit();
         }
-    }
-
-    public static void main(String[] args) throws Exception {
-        SampleTest test = new SampleTest();
-
-        // Run tests on Chrome
-        test.setUp();
-        test.testJoinTheSpot();
-        test.tearDown();
     }
 }
