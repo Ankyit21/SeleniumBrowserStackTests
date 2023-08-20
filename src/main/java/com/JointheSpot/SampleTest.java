0bc11e5f94a00d9962075b93bdfa77733b50ce04
ANKYIT21 <sanjeetbhandari21@gmail.com>
2023-08-20 17:00:41 +0545

    test

--- src/main/java/com/JointheSpot/SampleTest.java
+++ src/main/java/com/JointheSpot/SampleTest.java
@@ -10,22 +10,14 @@ import org.openqa.selenium.WebElement;
 public class SampleTest {
     private WebDriver driver;
 
-    public void setUp(String browser, String os, String osVersion) throws Exception {
+    public void setUp() throws Exception {
         DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability("browser", browser);
+        caps.setCapability("browser", "Chrome");
         caps.setCapability("browser_version", "latest");
-        caps.setCapability("os", os);
-        caps.setCapability("os_version", osVersion);
+        caps.setCapability("os", "Windows");
+        caps.setCapability("os_version", "10");
 
-        // Set capability for iOS distribution
-        if (os.equalsIgnoreCase("iOS")) {
-            caps.setCapability("browserstack.appstore_distribution", "public");
-        }
-        if (os.equalsIgnoreCase("Android")) {
-            caps.setCapability("browserstack.appstore_distribution", "public");
-        }
-
-        // Initialize WebDriver for specified browser and platform
+        // Initialize WebDriver for Chrome on Windows
         driver = new RemoteWebDriver(new URL("https://hub.browserstack.com/wd/hub"), caps);
     }
 
@@ -33,9 +25,9 @@ public class SampleTest {
         // Navigate to jointhespot.com
         driver.get("https://dev.d1oh6kkp7wvoqq.amplifyapp.com");
 
-       WebElement element = driver.findElement(By.xpath("//a[@data-bs-toggle='modal' and @data-bs-target='#login-popup' and contains(@class, 'btn-login') and text()='Login']"));
+        WebElement element = driver.findElement(By.xpath("//a[@data-bs-toggle='modal' and @data-bs-target='#login-popup' and contains(@class, 'btn-login') and text()='Login']"));
         element.click();
-        System.out.println("element clicked");
+        System.out.println("Element clicked");
 
     }
 
@@ -49,27 +41,7 @@ public class SampleTest {
         SampleTest test = new SampleTest();
 
         // Run tests on Chrome
-        test.setUp("Chrome", "Windows", "10");
-        test.testJoinTheSpot();
-        test.tearDown();
-
-        // Run tests on Safari
-        test.setUp("Safari", "OS X", "Big Sur");
-        test.testJoinTheSpot();
-        test.tearDown();
-
-        // Run tests on Firefox
-        test.setUp("Firefox", "Windows", "10");
-        test.testJoinTheSpot();
-        test.tearDown();
-
-        // Run tests on iOS
-        test.setUp("iPhone", "iOS", "14");
-        test.testJoinTheSpot();
-        test.tearDown();
-
-        // Run tests on Android
-        test.setUp("Android", "Android", "11.0");
+        test.setUp();
         test.testJoinTheSpot();
         test.tearDown();
     }
